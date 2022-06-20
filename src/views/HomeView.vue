163c589f50246ff7c69fc962388ea6c32214bb04
zicheng99 <lzc990403@gmail.com>
2022-06-20 22:06:03 +0800

    add timer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <div>Test Cloud Function</div>
     <div>{{ getCurrent }}</div>
+    <div>Timer: {{ timer }}</div>
     <div>Click Up Data</div>
     <div>{{ getResp }}</div>
   </div>
@@ -11,6 +12,7 @@
 import Axios from "axios";
 import { onBeforeMount, ref, computed } from "vue";
 
+const timer = ref<number>(0);
 const resp = ref("");
 const now = ref(new Date());
 async function getTasks(id: string) {
@@ -34,5 +36,12 @@ onBeforeMount(async () => {
   console.log("res.data", res.data);
   console.log("res.data.myData", JSON.parse(res.data.myData));
   resp.value = res.data;
+  startTimer();
 });
+
+function startTimer() {
+  setTimeout(() => {
+    timer.value += 1;
+  }, 1000);
+}
 </script>
