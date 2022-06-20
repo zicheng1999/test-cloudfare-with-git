a8d259a28dbfea0733a93614158bcfe7456fc8f3
zicheng99 <lzc990403@gmail.com>
2022-06-20 21:57:36 +0800

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <div>Test Cloud Function</div>
     <div>{{ getCurrent }}</div>
+    <div>{{ getResp }}</div>
   </div>
 </template>
 
@@ -9,6 +10,7 @@
 import Axios from "axios";
 import { onBeforeMount, ref, computed } from "vue";
 
+const resp = ref("");
 const now = ref(new Date());
 async function getTasks(id: string) {
   console.log("id", id);
@@ -22,9 +24,14 @@ const getCurrent = computed(() => {
   return now.value;
 });
 
+const getResp = computed(() => {
+  return resp.value;
+});
+
 onBeforeMount(async () => {
   const res = await getTasks("testid_001");
   console.log("res.data", res.data);
   console.log("res.data.myData", JSON.parse(res.data.myData));
+  resp.value = res.data;
 });
 </script>
