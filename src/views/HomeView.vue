f3c4dc850ed1bddddb33ea3ec23e12a6db7f227b
zicheng99 <lzc990403@gmail.com>
2022-06-20 20:24:08 +0800

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,4 +4,19 @@
   </div>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+import Axios from "axios";
+import { onBeforeUnmount } from "vue";
+
+async function getTasks(id: string) {
+  const result = await Axios.get("/api/test", {
+    params: { id: id },
+  });
+  return result;
+}
+
+onBeforeUnmount(async () => {
+  console.log("in onBeforeMount");
+  await getTasks("test id");
+});
+</script>
