26b7f75339344d1d16096beae9dbf36f35d78562
zicheng99 <lzc990403@gmail.com>
2022-06-20 19:41:57 +0800

    edit cf func

--- functions/api/test.ts
+++ functions/api/test.ts
@@ -10,12 +10,12 @@ export async function onRequest(context) {
     data, // arbitrary space for passing data between middlewares
   } = context;
 
-  const res = await fetch(
-    `https://rickandmortyapi.com/api/character/${params.id}`
-  );
-  const redJson = await res.json();
-  const info = JSON.stringify(redJson);
-  return new Response(info);
-
-  // return new Response(env.CLICK_UP_TOKEN);
+  const res = await Axios.get(`https://rickandmortyapi.com/api/character/1`);
+  const resJson = await res.data();
+  const result = {
+    data: resJson,
+    env: env.CLICK_UP_TOKEN,
+  };
+  const info = JSON.stringify(result);
+  return new Response(result);
 }
