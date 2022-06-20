872b9c9bc4200486448ae720521bba5cf2f34923
zicheng99 <lzc990403@gmail.com>
2022-06-20 18:09:47 +0800

    test cloud function

---
+++ functions/api/test.ts
@@ -0,0 +1,23 @@
+import Axios from "axios";
+export async function onRequest(context) {
+  // Contents of context object
+  const {
+    request, // same as existing Worker API
+    env, // same as existing Worker API
+    params, // if filename includes [id] or [[path]]
+    waitUntil, // same as ctx.waitUntil in existing Worker API
+    next, // used for middleware or to fetch assets
+    data, // arbitrary space for passing data between middlewares
+  } = context;
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify({
+      data: "from test.ts",
+    }),
+  };
+}
