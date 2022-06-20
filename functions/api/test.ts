3c91634b251d2160599197d29bc1443c3b79be53
zicheng99 <lzc990403@gmail.com>
2022-06-20 20:45:54 +0800

    test

--- functions/api/test.ts
+++ functions/api/test.ts
@@ -7,6 +7,10 @@ export async function onRequest(context) {
   try {
     return new Response(
       JSON.stringify({
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
         token: token,
         other: "test data 0806",
         para: params,
@@ -16,6 +20,10 @@ export async function onRequest(context) {
   } catch (e) {
     return new Response(
       JSON.stringify({
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
         token: token,
         para: params.id,
         error: e,
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,18 +4,21 @@
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import Axios from "axios";
-import { onBeforeUnmount } from "vue";
+import { onBeforeMount } from "vue";
 
 async function getTasks(id: string) {
-  const result = await Axios.get("/api/test", {
-    params: { id: id },
-  });
+  const result = await Axios.get(
+    "https://test-cloudfare-with-git.pages.dev/api/test",
+    {
+      params: { id: id },
+    }
+  );
   return result;
 }
 
-onBeforeUnmount(async () => {
+onBeforeMount(async () => {
   console.log("in onBeforeMount");
   const res = await getTasks("test id");
   console.log("res", res);
