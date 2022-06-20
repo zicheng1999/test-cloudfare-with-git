66fe306a499f0275a833b2c35aae42d1525f4b83
zicheng99 <lzc990403@gmail.com>
2022-06-20 20:14:45 +0800

    test

--- functions/api/test.ts
+++ functions/api/test.ts
@@ -1,39 +1,22 @@
 import Axios from "axios";
-interface IFilterDealerData {
-  field_id: string;
-  operator: string;
-  value: string;
-}
+
 export async function onRequest(context) {
   const { env, params } = context;
+  const token = env.CLICK_UP_TOKEN;
 
   try {
-    const taskListId = "152199949";
-    const apiGetTasks = `https://api.clickup.com/api/v2/list/${taskListId}/task`;
-
-    const dealerId = "c89f3d59-05a1-4fc5-9875-50b3294af7b5";
-
-    const token = env.CLICK_UP_TOKEN;
-    // const config = {
-    //   headers: {
-    //     Authorization: token ? token : "",
-    //   },
-    // };
-
-    // const tasks = (await Axios.get(apiGetTasks, config)).data;
-
     return new Response(
       JSON.stringify({
-        token: env.CLICK_UP_TOKEN,
+        token: token,
         other: "test data 0806",
-        // tasks: tasks,
-        para: params.id,
+        para: params,
+        id: params.id,
       })
     );
   } catch (e) {
     return new Response(
       JSON.stringify({
-        token: env.CLICK_UP_TOKEN,
+        token: token,
         para: params.id,
         error: e,
       })
