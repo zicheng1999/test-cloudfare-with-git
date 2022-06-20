5edc235dada65e320679782d4182c57eda6ee14a
zicheng99 <lzc990403@gmail.com>
2022-06-20 21:08:31 +0800

    test

--- functions/api/test.ts
+++ functions/api/test.ts
@@ -3,7 +3,7 @@ import Axios from "axios";
 export async function onRequest(context) {
   const { env, params } = context;
   const token = env.CLICK_UP_TOKEN;
-
+  const myData = await (await Axios.get("https://catfact.ninja/fact")).data;
   try {
     return new Response(
       JSON.stringify({
@@ -11,16 +11,12 @@ export async function onRequest(context) {
         other: "test data 0806",
         para: params,
         id: params.id || "no id gotten",
-        cat: (await Axios.get("https://catfact.ninja/fact")).data,
+        cat: myData,
       })
     );
   } catch (e) {
     return new Response(
       JSON.stringify({
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Credentials": true,
-        },
         token: token,
         para: params.id,
         error: e,
