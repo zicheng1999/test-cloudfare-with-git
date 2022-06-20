bbc75d5269d60e2e2e7fd4e387c13ca98ecccc08
zicheng99 <lzc990403@gmail.com>
2022-06-20 20:11:18 +0800

    test

--- functions/api/test.ts
+++ functions/api/test.ts
@@ -14,11 +14,11 @@ export async function onRequest(context) {
     const dealerId = "c89f3d59-05a1-4fc5-9875-50b3294af7b5";
 
     const token = env.CLICK_UP_TOKEN;
-    const config = {
-      headers: {
-        Authorization: token ? token : "",
-      },
-    };
+    // const config = {
+    //   headers: {
+    //     Authorization: token ? token : "",
+    //   },
+    // };
 
     // const tasks = (await Axios.get(apiGetTasks, config)).data;
 
